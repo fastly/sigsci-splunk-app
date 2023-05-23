e175aa3bc51ce2db522b7b9bc5766b9fd72e3794
Douglas Coburn <douglas@dactbc.com>
2023-05-22 23:56:56 -0700

    Optimizations and fixed for possible duplications

--- sigsci_TA_for_splunk/README/inputs.conf.spec
+++ sigsci_TA_for_splunk/README/inputs.conf.spec
@@ -1,8 +1,8 @@
-[SigsciEvent://<name>]
-site_api_name = This is the Site API Name. It should not be a URL.
+[SigsciRequests://<name>]
+site_api_name = This is the API Name of the site to pull date from. This should not be a URL.
 
 [SigsciActivity://<name>]
 place_holder = It was required to have one option even if it isn't needed. You can skip this one.
 
-[SigsciRequests://<name>]
-site_api_name = This is the API Name of the site to pull date from. This should not be a URL.
+[SigsciEvent://<name>]
+site_api_name = This is the Site API Name. It should not be a URL.
--- sigsci_TA_for_splunk/bin/input_module_SigsciRequests.py
+++ sigsci_TA_for_splunk/bin/input_module_SigsciRequests.py
@@ -1,12 +1,6 @@
 # encoding = utf-8
-
-import time
-from datetime import datetime, timedelta
 from timeit import default_timer as timer
-import requests
-import calendar
-import json
-from sigsci_helper import check_response, get_request_data
+from sigsci_helper import get_from_and_until_times, Config, get_results, get_until_time
 
 '''
     IMPORTANT
@@ -53,7 +47,8 @@ def validate_input(helper, definition):
 
 def collect_events(helper, ew):
     start = timer()
-    # loglevel = helper.get_log_level()
+    loglevel = helper.get_log_level()
+    helper.set_log_level(loglevel)
     # Proxy setting configuration
     # proxy_settings = helper.get_proxy()
     global_email = helper.get_global_setting("email")
@@ -62,28 +57,40 @@ def collect_events(helper, ew):
     api_host = 'https://dashboard.signalsciences.net'
     helper.log_info("email: %s" % global_email)
     helper.log_info("corp: %s" % global_corp_api_name)
-    python_requests_version = requests.__version__
-    user_agent_version = "1.0.26"
-    user_agent_string = (
-        f"TA-sigsci-waf/{user_agent_version} "
-        f"(PythonRequests {python_requests_version})"
-    )
 
     def pull_requests(current_site, delta, key=None):
         site_name = current_site
-        until_time = datetime.utcnow() - timedelta(minutes=5)
-        until_time = until_time.replace(second=0, microsecond=0)
-        from_time = until_time - timedelta(seconds=delta)
-        until_time = calendar.timegm(until_time.utctimetuple())
-        from_time = calendar.timegm(from_time.utctimetuple())
-        from_time_friendly = datetime.fromtimestamp(from_time)
-        until_time_friendly = datetime.fromtimestamp(until_time)
+        last_name = f"requests_last_until_time_{current_site}"
+        last_run_until = helper.get_check_point(last_name)
+        helper.log_info(f"last_run_until: {last_run_until}")
+        if last_run_until is None:
+            (
+                until_time,
+                from_time,
+                from_time_friendly,
+                until_time_friendly
+            ) = get_from_and_until_times(delta, five_min_offset=True)
+        else:
+            (
+                until_time,
+                from_time,
+                from_time_friendly,
+                until_time_friendly
+            ) = get_until_time(last_run_until, delta, five_min_offset=True)
+        if from_time is None:
+            helper.log_info(f"{last_run_until} >= current now time, skipping run")
+            return
+        if from_time >= until_time:
+            helper.save_check_point(last_name, from_time)
+            helper.log_info(
+                f"from_time {from_time} >= until_time {until_time}, skipping run"
+            )
+            return
+        helper.save_check_point(last_name, until_time)
         helper.log_info("SiteName: %s" % site_name)
 
         helper.log_info(f"Start Period: {from_time_friendly}")
         helper.log_info(f"End Period: {until_time_friendly}")
-
-
         input_name = helper.get_input_stanza_names()
         single_name = ""
 
@@ -99,123 +106,35 @@ def collect_events(helper, ew):
             helper.log_info(f"single_name: {single_name}")
 
         # Loop across all the data and output it in one big JSON object
-        headers = {
-            'Content-type': 'application/json',
-            'x-api-user': global_email,
-            'x-api-token': global_api_token,
-            'User-Agent': user_agent_string
-        }
-
         url = (
             f"{api_host}/api/v0/corps/{global_corp_api_name}"
             f"/sites/{site_name}/feed/requests?"
             f"from={from_time}&until={until_time}"
         )
-        loop = True
-
-        counter = 1
         helper.log_info("Pulling requests from requests API")
-        all_requests = []
-        while loop:
-            helper.log_info("Processing page %s" % counter)
-            start_page = timer()
-            response_result, response_code, response_error = \
-                get_request_data(url, headers, helper)
-
-            pulled, request_details = check_response(
-                response_code,
-                response_error,
-                global_email=global_email,
-                global_corp_api_name=global_corp_api_name,
-                current_site=site_name,
-                from_time=from_time,
-                until_time=until_time
-            )
-
-            if not pulled and request_details["msg"] != "rate-limit":
-                helper.log_error("Failed to pull results")
-                helper.log_error(request_details)
-                exit()
-            if not pulled and request_details["msg"] == "rate-limit":
-                helper.log_error("Rate Limit hit")
-                helper.log_error("Retrying in 10 seconds")
-                time.sleep(10)
-                break
-            else:
-                response = response_result
-
-            number_requests_per_page = len(response['data'])
-            helper.log_info(
-                f"Number of Requests for Page: {number_requests_per_page}"
-            )
-
-            for request in response['data']:
-                data = json.dumps(request)
-                data = json.loads(data)
-                headers_to_fix = {
-                    'headersOut': data['headersOut'],
-                    'headersIn': data['headersIn']
-                }
-
-                new_format_out = {}
-
-                if not (headers_to_fix['headersOut'] is None):
-                    for out in headers_to_fix['headersOut']:
-                        new_format_out[out[0]] = out[1]
-
-                    data['headersOut'] = new_format_out
-
-                new_format_in = {}
-
-                if not (headers_to_fix['headersIn'] is None):
-                    for header_in in headers_to_fix['headersIn']:
-                        headers_to_fix[header_in[0]] = header_in[1]
-
-                    data['headersIn'] = headers_to_fix
-
-                data = json.dumps(data)
-
-                # helper.log_debug("%s" % data)
-                all_requests.append(data)
-
-            if "next" in response and "uri" in response['next']:
-                next_url = response['next']['uri']
-                if next_url == '':
-                    helper.log_info("Finished Page %s" % counter)
-                    counter += 1
-                    end_page = timer()
-                    page_time = end_page - start_page
-                    page_time_result = round(page_time, 2)
-                    helper.log_info(
-                        f"Total Page Time: {page_time_result} seconds"
-                    )
-                    loop = False
-                else:
-                    url = api_host + next_url
-                    helper.log_info("Finished Page %s" % counter)
-                    counter += 1
-                    end_page = timer()
-                    page_time = end_page - start_page
-                    page_time_result = round(page_time, 2)
-                    helper.log_info(
-                        f"Total Page Time: {page_time_result} seconds"
-                    )
-            else:
-                loop = False
-
+        config = Config(
+            url=url,
+            api_host=api_host,
+            from_time=from_time,
+            until_time=until_time,
+            global_email=global_email,
+            global_corp_api_name=global_corp_api_name,
+            current_site=current_site,
+        )
+        config.headers = {
+            'Content-type': 'application/json',
+            'x-api-user': global_email,
+            'x-api-token': global_api_token,
+            'User-Agent': config.user_agent_string
+        }
+        all_requests = get_results("Requests", helper, config)
         total_requests = len(all_requests)
         helper.log_info("Total Requests Pulled: %s" % total_requests)
         write_start = timer()
         for current_event in all_requests:
-            # helper.log_debug(current_event)
-            # helper.log_info(f"data={event_data}")
             if key is None:
-                # helper.log_info("input_type=single")
                 source_index = helper.get_output_index()
                 source_type = helper.get_sourcetype()
-                # helper.log_info(f"source={single_name}")
-                # helper.log_info(f"index={source_index}")
-                # helper.log_info(f"source_type={source_type}")
                 event = helper.new_event(
                     source=single_name,
                     index=helper.get_output_index(),
